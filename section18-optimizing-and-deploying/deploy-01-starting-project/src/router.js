c2e9e3a7f228f592e8fcd337ab44f2d5d9d6267d
nguyenquochop <nguyenquochop23@gmail.com>
2023-10-27 16:17:47 +0700

    reusing functionality, mixin and custom composition function

--- section18-optimizing-and-deploying/deploy-01-starting-project/src/router.js
+++ section18-optimizing-and-deploying/deploy-01-starting-project/src/router.js
@@ -1,7 +1,7 @@
-// import { defineAsyncComponent } from 'vue';
+import { defineAsyncComponent } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
-import CoachDetail from './pages/coaches/CoachDetail.vue';
+// import CoachDetail from './pages/coaches/CoachDetail.vue';
 import CoachesList from './pages/coaches/CoachesList.vue';
 import CoachRegistation from './pages/coaches/CoachRegistration.vue';
 import ContactCoach from './pages/requests/ContactCoach.vue';
@@ -9,6 +9,10 @@ import RequestsReceived from './pages/requests/RequestsReceived.vue';
 import NotFound from './pages/NotFound.vue';
 import UserAuth from './pages/auth/UserAuth.vue';
 import store from './store/index.js';
+
+const CoachDetail = defineAsyncComponent(() =>
+  import('./pages/coaches/CoachDetail.vue')
+);
 
 const router = createRouter({
   history: createWebHistory(),
@@ -20,17 +24,25 @@ const router = createRouter({
       component: CoachDetail,
       props: true,
       children: [
-        { path: 'contact', component: ContactCoach } // /coaches/c1/contact
-      ]
+        { path: 'contact', component: ContactCoach }, // /coaches/c1/contact
+      ],
     },
-    { path: '/register', component: CoachRegistation, meta: { requiresAuth: true } },
-    { path: '/requests', component: RequestsReceived, meta: { requiresAuth: true } },
+    {
+      path: '/register',
+      component: CoachRegistation,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/requests',
+      component: RequestsReceived,
+      meta: { requiresAuth: true },
+    },
     { path: '/auth', component: UserAuth, meta: { requiresUnauth: true } },
-    { path: '/:notFound(.*)', component: NotFound }
-  ]
+    { path: '/:notFound(.*)', component: NotFound },
+  ],
 });
 
-router.beforeEach(function(to, _, next) {
+router.beforeEach(function (to, _, next) {
   if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
     next('/auth');
   } else if (to.meta.requiresUnauth && store.getters.isAuthenticated) {
