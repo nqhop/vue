b3d56f8e3b2bffff0d961ab8503f508492663097
Nguyễn Quốc Hợp <hop35889@gmail.com>
2024-01-15 17:12:25 +0700

    composition API, typescript, luxon, bulma

---
+++ vuejs-3-the-composition-API-(inc-script-setup-typescript)/my-app/src/App.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import TimeLine from "./components/TimeLine.vue";
+</script>
+
+<template>
+  <div class="section">
+    <div class="container">
+      <TimeLine />
+    </div>
+  </div>
+</template>
+
+<style>
+@import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css";
+</style>
